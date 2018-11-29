b0c4d06486915be9ab4a54ec4535074568b24516
H607678 <Jeffrey.R.Guzman@kp.org>
2018-11-28 16:14:39 -0800

    checking response from slack

--- vars/getSlackuser.groovy
+++ vars/getSlackuser.groovy
@@ -6,6 +6,7 @@ import groovy.json.JsonOutput
 def call(Email) {
   def param = "email=${Email.encodeURL()}"
   def m = sh(returnStdout: true, script: "curl --silent -X POST -H 'Authorization: Bearer ${env.SLACK_TOKEN}' -H \"Content-Type: application/x-www-form-urlencoded\" --data \'${param}\' ${env.SLACK_WEBHOOK_URL}/api/users.lookupByEmail").trim() 
+  echo m
   def json = readJSON text: m
 
   return json
