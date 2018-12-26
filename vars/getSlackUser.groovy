41fbf38483292e35a4c798d330abff635e14cebc
JGuzman4 <jeffg@liatrio.com>
2018-12-26 15:02:02 -0800

    passing in build user email

--- vars/getSlackUser.groovy
+++ vars/getSlackUser.groovy
@@ -5,10 +5,12 @@ import groovy.json.JsonOutput
 import java.net.URLEncoder
 
 def call(Email) {
-  sh "env"
-  def email = java.net.URLEncoder.encode(Email, "UTF-8")
-  def param = "email=${email}"
-  def m = sh(returnStdout: true, script: "curl --silent -X POST -H 'Authorization: Bearer ${env.SLACK_TOKEN}' -H \"Content-Type: application/x-www-form-urlencoded\" --data \'${param}\' ${env.SLACK_WEBHOOK_URL}/api/users.lookupByEmail").trim() 
+  //echo Email
+  //def email = java.net.URLEncoder.encode(Email, "UTF-8")
+  //echo email
+  //def param = "email=${email}"
+  def param = "email=${Email}"
+  def m = sh(returnStdout: true, script: "curl -G --silent -X GET -H 'Authorization: Bearer ${env.SLACK_TOKEN}' -H \"Content-Type: application/x-www-form-urlencoded\" --data-urlencode \"${param}\" ${env.SLACK_WEBHOOK_URL}/api/users.lookupByEmail").trim() 
   echo m
   def json = readJSON text: m
 
