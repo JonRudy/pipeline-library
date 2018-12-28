6f45b81bb07bfd5e1842254817fc7993bd1afa10
JGuzman4 <jeffg@liatrio.com>
2018-12-28 12:59:32 -0800

    trying to see what vars are available

--- vars/getSlackUser.groovy
+++ vars/getSlackUser.groovy
@@ -10,6 +10,7 @@ def call() {
     email = "${env.BUILD_USER_EMAIL}"
   else if ("${env.CHANGE_AUTHOR_EMAIL}" != "")
     email = "${env.CHANGE_AUTHOR_EMAIL}"
+  sh "env"
   def param = "email=${email}"
   def m = sh(returnStdout: true, script: "curl -G --silent -X GET -H 'Authorization: Bearer ${env.SLACK_TOKEN}' -H \"Content-Type: application/x-www-form-urlencoded\" --data-urlencode \"${param}\" ${env.SLACK_WEBHOOK_URL}/api/users.lookupByEmail").trim() 
   def json = readJSON text: m
